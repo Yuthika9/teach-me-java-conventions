40703f64f049ac2bb65600c9fcba46d3e9e33144
Yuthika9 <167095094+Yuthika9@users.noreply.github.com>
2024-04-19 15:40:14 +0530

    Update powertest.java

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -1,46 +1,40 @@
-package powerpackage;
-
-
 import org.junit.Test;
+import static org.junit.Assert.*;
 
- @Test
-        public void shouldReturnOneIfTheExponentIsOne(){
-            int exponent = 1;
-            int index = 1;
-            int ans = 1;
+@Test
+public void shouldReturnIndexIfTheExponentIsOne(){
 
-            Power power = new Power(index,exponent);
+    int exponent = 1;
+    int index = 1;
 
-            assert power.findPower() == ans;
-        }
+    Power power = new Power(index,exponent);
 
-        @Test
-        public void shouldReturnOneIfTheExponentIsZero(){
-            int exponent = 0;
-            int index = 4;
-            int ans = 1;
-            Power power = new Power(index,exponent);
+    assertEquals(1,power.calculate());
+}
 
-            assert power.findPower() == ans;
-        }
-        @Test
-        public void shouldReturnTwoIfTheBaseIsTwoExponentOne(){
-            int exponent = 1;
-            int index = 2;
-            int ans = 2;
+@Test
+public void shouldReturnOneIfTheExponentIsZero(){
 
-            Power power = new Power(index,exponent);
-            assert power.findPower() == ans;
+    int exponent = 0;
+    int index = 4;
 
-        }
+    Power power = new Power(index,exponent);
 
-        @Test
-        public void shouldReturnNineIfTheBaseIsThreeExponentThree(){
-            int exponent = 3;
-            int index = 3;
-            int ans = 9;
+    assertEquals(1,power.calculate());
+}
 
-            Power power = new Power(index,exponent);
-            assert power.findPower() == ans;
+@Test
+public void shouldReturnNineIfTheBaseIsThreeExponentThree(){
+
+    int exponent = 3;
+    int index = 3;
+
+
+    Power power = new Power(index,exponent);
+    assertEquals(9,power.calculate());
 
 }
+
+
+
+
